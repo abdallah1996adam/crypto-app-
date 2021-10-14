57907cfb9a7a8bac1348d7e72e962c86381a30ae
abdallah1996adam <abdallahaadam@protonmail.com>
2021-10-14 18:32:42 +0200

    showing coins :tada:

--- src/pages/cryptoCurrencies/CryptoCrunnencies.jsx
+++ src/pages/cryptoCurrencies/CryptoCrunnencies.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Row, Card, Input, Col } from "antd";
+import { useGetCryptosQuery } from "../../services/crypyoApi";
 
-const CryptoCrunnencies = () => {
-    return (
-        <div>
-            cryptocurrencies
-        </div>
-    )
-}
+const CryptoCrunnencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  const [crypto, setCryptos] = useState(cryptoList?.data?.coins);
 
-export default CryptoCrunnencies
+
+  if(isFetching) return "loading......."
+
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {crypto?.map((coin) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={coin.id}>
+            <Link to={`/crypto/${coin.id}`}>
+              <Card
+                title={`${coin.rank}. ${coin.name}`}
+                extra={<img className="crypto-image" src={coin.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(coin.price)}</p>
+                <p>Market Cap: {millify(coin.marketCap)} </p>
+                <p>Daily change: {millify(coin.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default CryptoCrunnencies;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,15 @@ import millify from "millify";
 import { Statistic, Typography, Row, Col } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../../services/crypyoApi";
+import CryptoCrunnencies from "../cryptoCurrencies/CryptoCrunnencies";
+import { News } from "..";
 
 export const Home = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+
+  if (isFetching) return "loading..........";
+
+  const globalStats = data?.data?.stats;
   return (
     <>
       <Typography.Title level={5} className="heading">
@@ -12,21 +19,54 @@ export const Home = () => {
       </Typography.Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5"></Statistic>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={globalStats.total}
+          ></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5"></Statistic>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          ></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5"></Statistic>
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          ></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5"></Statistic>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          ></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5"></Statistic>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          ></Statistic>
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Typography.Title>
+      </div>
+      <CryptoCrunnencies simplified />
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Latest Crypto News
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Typography.Title>
+      </div>
+      <News simplified />
     </>
   );
 };
